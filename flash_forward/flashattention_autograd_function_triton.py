419386cf6900459b995259275f3a14711b8040aa
Lewis Won <lewis.won@gmail.com>
2026-07-22 22:07:11 +0800

    initialised triton

---
+++ flash_forward/flashattention_autograd_function_triton.py
@@ -0,0 +1,87 @@
+import triton
+import triton.language as tl
+@triton.jit
+def flash_fwd_kernel(
+    Q_ptr, K_ptr, V_ptr,
+    O_ptr, L_ptr,
+    stride_qb, stride_qq, stride_qd,
+    stride_kb, stride_kk, stride_kd,
+    stride_vb, stride_vk, stride_vd,
+    stride_ob, stride_oq, stride_od,
+    stride_lb, stride_lq,
+    N_QUERIES, N_KEYS,
+    scale,
+    D: tl.constexpr,
+    Q_TILE_SIZE: tl.constexpr,
+    K_TILE_SIZE: tl.constexpr,
+):
+    # Program indices
+    query_tile_index = tl.program_id(0)
+    batch_index = tl.program_id(1)
+
+    # Offset each pointer with the corresponding batch index
+    # multiplied with the batch stride for each tensor
+    # Each program only takes a slice of Q_block, with query_tile_index * Q_TILE_SIZE offset
+    Q_block_ptr = tl.make_block_ptr(
+        Q_ptr + batch_index * stride_qb,
+        shape=(N_QUERIES, D),
+        strides=(stride_qq, stride_qd),
+        offsets=(query_tile_index * Q_TILE_SIZE, 0),
+        block_shape=(Q_TILE_SIZE, D),
+        order=(1, 0)
+    )
+    # Each program processes the entire K_block, hence offset begins at (0,0)
+    K_block_ptr = tl.make_block_ptr(
+        K_ptr + batch_index * stride_kb,
+        shape=(N_KEYS, D),
+        strides=(stride_kk, stride_kd),
+        offsets=(0, 0),
+        block_shape=(K_TILE_SIZE, D),
+        order=(1,0)
+    )
+    # Use K_TILE_SIZE as the inner j loop always load the K tile and V tile together for the same key block
+    # Each program processes the entire V_block, hence offset begins at (0,0)
+    V_block_ptr = tl.make_block_ptr(
+        V_ptr + batch_index * stride_vb,
+        shape=(N_KEYS, D),
+        strides=(stride_vk, stride_vd),
+        offsets=(0, 0),
+        block_shape=(K_TILE_SIZE, D),
+        order=(1, 0)
+    )
+    O_block_ptr = tl.make_block_ptr(
+        O_ptr + batch_index * stride_ob,
+        shape=(N_QUERIES, D),
+        strides=(stride_oq, stride_od),
+        offsets=(query_tile_index * Q_TILE_SIZE, 0),
+        block_shape=(Q_TILE_SIZE, D),
+        order=(1,0)
+    )
+    L_block_ptr = tl.make_block_ptr(
+        L_ptr + batch_index * stride_lb,
+        shape=(N_QUERIES,),
+        strides=(stride_lq,),
+        offsets=(query_tile_index * Q_TILE_SIZE,),
+        block_shape=(Q_TILE_SIZE,),
+        order=(0,)
+    )
+    # Initialise the on-chip accumulators
+    # Accumulate in fp32 for numerical stability
+    O_i0 = tl.zeros(
+        shape=(Q_TILE_SIZE, D), 
+        dtype=tl.float32
+    )
+    l_i0 = tl.zeros(
+        shape=(Q_TILE_SIZE,), 
+        dtype=tl.float32
+    )
+    m_i0 = tl.full(
+        shape=(Q_TILE_SIZE,), 
+        value=-float('inf'), 
+        dtype=tl.float32
+    )
+    Q_block = tl.load(
+        pointer=Q_block_ptr, 
+        boundary_check=(0,1),
+        padding_option="zero"
+    )
